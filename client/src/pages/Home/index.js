7c8de5820bdbe6c2e917d78805308344594df098
Ellis-98 <pierre.schaefer@epitech.eu>
2021-07-15 00:07:20 +0200

    Ajout de la structure pour le Front-End

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import '../../App.css'
+import Header from '../../components/CommonComponents/Header/Header'
+
+function Home () {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
