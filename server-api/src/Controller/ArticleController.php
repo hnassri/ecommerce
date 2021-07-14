7a7e658a0f181fb7e9faacd89ecbbacb2dd36a39
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-07-14 20:59:16 +0200

    api return details of one article

--- server-api/src/Controller/ArticleController.php
+++ server-api/src/Controller/ArticleController.php
@@ -33,4 +33,37 @@ class ArticleController extends AbstractController
         
         return $this->json($items);
     }
+
+    #[Route('/article/{id}', name: 'article_show')]
+    public function show(int $id): Response
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+        if(empty($article)) {
+            return $this->json(["message" => "This product doesn't exist!"]);
+        }
+        $items = [];
+        $stock = ($article->getQuantity() > 0) ? "stock" : "empty";
+        $features = $this->getFeatures($article);
+        $items[] = [
+            "name" => $article->getName(),
+            "price" => $article->getPrice(),
+            "description" =>  $article->getDescription(),
+            "stock" => $stock,
+            "uuid" => $article->getUuid(),
+            "features" => $features
+        ];
+        
+        return $this->json($items);
+    }
+
+    protected function getFeatures(Article $article)
+    {
+        $global_features = $this->getDoctrine()->getRepository(GlobalFeature::class)->findBy(["article_id" => $article->getId()]);
+        $features = [];
+        foreach($global_features as $global_feature){
+            $feature = $this->getDoctrine()->getRepository(Feature::class)->find($global_feature->getFeatureId());
+            $features[] = $feature->getName();
+        }
+        return $features;
+    }
 }
