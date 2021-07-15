2f0e0ef71bef0c4cc28422516481b5d54c5f73d3
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-07-15 16:22:58 +0200

    api update category done

--- server-api/src/Controller/CategoryController.php
+++ server-api/src/Controller/CategoryController.php
@@ -59,4 +59,37 @@ class CategoryController extends AbstractController
             "message" => "An error occured, this category has not be created!"
         ]);
     }
+
+    #[Route('/category/edit/{id}', name: 'category_edit', methods: ["PUT"])]
+    public function update(Request $request, int $id): Response
+    {
+        $request_name = trim($request->get("name"));
+        if(empty($request_name)){
+            return $this->json([
+                "success" => false,
+                "message" => "Please give a name, so we can update the category!"
+            ]);
+        }
+        $category = $this->getDoctrine()->getRepository(Category::class)->findByName($request_name);
+        if(empty($category) == false){
+            return $this->json([
+                "success" => false,
+                "message" => "This category already exist!"
+            ]);
+        }
+        $entityManager = $this->getDoctrine()->getManager();
+        $category = new Category();
+        $category->setName($request_name);
+        $entityManager->persist($category);
+        if($entityManager->flush() !== false){
+            return $this->json([
+                "success" => true,
+                "message" => "Category updated!"
+            ]);
+        }
+        return $this->json([
+            "success" => false,
+            "message" => "An error occured, this category has not be updated!"
+        ]);
+    }
 }
