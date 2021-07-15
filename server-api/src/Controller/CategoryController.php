34c372452412d2be489d42e3dc858d44b374d788
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-07-15 15:54:33 +0200

    api create category done

--- server-api/src/Controller/CategoryController.php
+++ server-api/src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Category;
@@ -24,5 +25,38 @@ class CategoryController extends AbstractController
         }
         
         return $this->json($items);
+    }
+
+    #[Route('/category_new', name: 'category_create', methods: ["POST"])]
+    public function create(Request $request): Response
+    {
+        $request_name = trim($request->get("name"));
+        if(empty($request_name)){
+            return $this->json([
+                "success" => false,
+                "message" => "Please, give a name for the category!"
+            ]);
+        }
+        $category = $this->getDoctrine()->getRepository(Category::class)->findByName($request_name);
+        if(empty($category) == false){
+            return $this->json([
+                "success" => false,
+                "message" => "This category already exist!"
+            ]);
+        }
+        $entityManager = $this->getDoctrine()->getManager();
+        $category = new Category();
+        $category->setName($request_name);
+        $entityManager->persist($category);
+        if($entityManager->flush() !== false){
+            return $this->json([
+                "success" => true,
+                "message" => "Category created!"
+            ]);
+        }
+        return $this->json([
+            "success" => false,
+            "message" => "An error occured, this category has not be created!"
+        ]);
     }
 }
