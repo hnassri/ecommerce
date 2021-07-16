3a62ce0447b0eea425404459810c35b98ab5ceb2
Mahamat Abakar <mahamat.abakar-hassan@epitech.eu>
2021-07-16 15:24:55 +0200

    finalisation de la partie front auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Navbar from './components/CommonComponents/Navbar/Navbar';
-import CardItems from './components/CommonComponents/CardItems/CardItems';
-import Carousel from './components/CommonComponents/BannerSlider/BannerSlider'
 import './App.css';
-
+import {HashRouter, Route, Switch} from 'react-router-dom'
+import Login from './pages/Login/index'
+import Home from './pages/Home/index'
+import Register from './pages/Register/index'
+import CardItems from './components/CommonComponents/CardItems/CardItems';
+import Carousel from './components/CommonComponents/BannerSlider/BannerSlider';
 const App = ()=>{
   return (
     <>
       <Navbar />
       <Carousel />
       <CardItems />
+      <HashRouter>
+             <Switch>
+              <Route exact path="/login" component={Login}/>
+              <Route exact path="/register" component={Register}/>
+             </Switch>
+      </HashRouter>
     </>
   );
 
