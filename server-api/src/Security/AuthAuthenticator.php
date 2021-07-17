eb084dcf4528296a214f88912eaa287feab9dc28
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-07-17 12:14:42 +0200

    a

--- server-api/src/Security/AuthAuthenticator.php
+++ server-api/src/Security/AuthAuthenticator.php
@@ -28,68 +28,77 @@ class AuthAuthenticator extends AbstractAuthenticator
 
     public function supports(Request $request): ?bool
     {
-        if ($request->attributes->has('_route')){
-            switch ($request->attributes->get('_route')){
-                // route proteger
-                case 'user_info':
-                    
-                   // only admin roles can access to api
+        if ($request->attributes->has('_route')) {
+            switch ($request->attributes->get('_route')) {
+                case 'user_all':
+                case 'user_info_admin':
+                case 'user_update_admin':
+                    // only admin roles can access to api
                     return $this->check_authorization($request, ['ROLE_ADMIN']);
                 case 'user_update':
+                case 'user_info':
+                    /*  */
                     return $this->check_authorization($request, ['*']);
                 default:
+
                     return false; // Authorized route by using without token
             }
         }
+
         return true;
     }
 
     public function authenticate(Request $request): PassportInterface
     {
         $user = $this->userRepository->findBy(array('email' => 'mohg'));
-        
-        if(!$user){
+
+        if (!$user) {
             throw new UserNotFoundException();
         }
-        return new Passport(new UserBadge($user[0]->getUsername()),new PasswordCredentials($request->request->get('psw')),[
-                 new PasswordUpgradeBadge($request->get('psw'),$this->userRepository)
+        return new Passport(new UserBadge($user[0]->getUsername()), new PasswordCredentials($request->request->get('psw')), [
+            new PasswordUpgradeBadge($request->get('psw'), $this->userRepository)
         ]);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         return new JsonResponse([
-            "message" => "ce code est la pour faire beau",
+            "message" => "ce code ne sera jamais executer "
         ], 401);
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         return new JsonResponse([
-            "success"=> false,
-            "error" => "access deny, make sure you have a valid jwt key and the necessary permissions",
+            "success" => false,
+            "error" => "accees deny, make sure you have a valid jwt key and the necessary permissions",
         ], 401);
     }
 
     private function check_authorization(Request $request, array $roles = ['ROLE_USER'])
     {
-            $token = str_replace('Bearer ', '', $request->headers->get('authorization'));
-            $token = trim($token);
-            
-            try {
-                $decoded = JWT::decode($token, "9d12902d04e3cf1a094644a04915f6ef", array('HS256'));
-                $request->attributes->set('infotoken', (array)$decoded);
-                if ($roles[0] === '*'){
-                    return false; // authorize request
-                } else {
-                    // if roles is different than specified role the request is refused.
-                    if ($decoded->role[0] !== $roles[0]){
-                        return true;
-                    }
-                }
-            } catch (\Exception $e) {
-                return true;
+        $token = str_replace('Bearer ', '', $request->headers->get('authorization'));
+        $token = trim($token);
+        try {
+            $decoded = JWT::decode($token, "9d12902d04e3cf1a094644a04915f6ef", array('HS256'));
+            $request->attributes->set('infotoken', (array)$decoded);
+            if($roles[0] == '*')
+            {
+                return false;
             }
-            return false;
+            else{
+                if ($decoded->role[0] !== $roles[0])
+            {
+                return true;
+            } else {
+                return false;
+            }
+            }
+            
+        } catch (\Exception $e) {
+            return true;
+        }
+
+        return false;
     }
 }
