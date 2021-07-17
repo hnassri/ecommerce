df72b99317a908061db90931ebf1ff3a4cc06fa6
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-07-17 20:27:18 +0200

    components login register

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import Navbar from './components/CommonComponents/Navbar/Navbar';
-import './App.css';
-import {HashRouter, Route, Switch} from 'react-router-dom'
-import Login from './pages/Login/index'
-import Home from './pages/Home/index'
-import Register from './pages/Register/index'
-import CardItems from './components/CommonComponents/CardItems/CardItems';
-import Carousel from './components/CommonComponents/BannerSlider/BannerSlider';
+
+
+import SetingUpFrom from './components/From/SignUpForm';
 const App = ()=>{
   return (
     <>
-      <Navbar />
-      <Carousel />
-      <CardItems />
-      <HashRouter>
-             <Switch>
-              <Route exact path="/login" component={Login}/>
-              <Route exact path="/register" component={Register}/>
-             </Switch>
-      </HashRouter>
+
+      <SetingUpFrom></SetingUpFrom>
+    
     </>
   );
 
