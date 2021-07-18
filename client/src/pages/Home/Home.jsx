7524087007e70efc760796f1b49562e7b83c2d64
Mahamat Abakar <mahamat.abakar-hassan@epitech.eu>
2021-07-18 02:18:16 +0200

    mis e n place de la partie video

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import React, { Component ,useState} from "react"
 import SliderHome from '../../components/Home/SliderHome/SlideHome'
 import Header from '../../components/CommonComponents/Header/Header'
 import Collection from '../../components/Home/Collection/Collection'
+import Video from '../../components/Home/Video/Video'
 
 
 
@@ -12,6 +13,10 @@ const Home = props => {
         <Header/>
          <SliderHome/>
          <Collection/>
+         <Video
+         title="Lorem ipsum dolor sit amet, consectetur adipisicing <br /> elit, sed do
+         <span>eiusmod tempor</span> incididunt."
+         video="https://player.vimeo.com/video/172601404?autoplay=1"/>
         </div>
     )
 }
