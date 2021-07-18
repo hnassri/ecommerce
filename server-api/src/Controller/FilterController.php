971473208c28fe750fad856726936b08197b8a19
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-07-18 23:47:56 +0200

    api: filter by category and by name together done

--- server-api/src/Controller/FilterController.php
+++ server-api/src/Controller/FilterController.php
@@ -21,43 +21,50 @@ class FilterController extends AbstractController
         $name = trim($request->query->get("name"));
         $category_name = trim($request->query->get("category"));
         $category = $this->getDoctrine()->getRepository(Category::class)->findOneByName($category_name);
-        $sql = "SELECT a.id, a.name, a.description, a.uuid, a.price, i.url FROM article a ";
-        $parameter = [];
-        if(empty($category)){
-            $sql .= "INNER JOIN image i ON a.id = i.article_id_id ";
-        }else{
-            $sql .= "INNER JOIN (global_category gc, image i) ON a.id = gc.article_id_id and gc.category_id_id = :category_id and a.id = i.article_id_id ";
-            $parameter[] = ['category_id' => $category->getId()];
+
+        $queryBuilder = $entityManager->createQueryBuilder();
+        $queryBuilder->select('a')
+        ->from(Article::class, 'a');
+
+        if(empty($category) === false){
+            $queryBuilder = $queryBuilder->innerJoin('a.globalCategories', 'c',  'WITH', 'c.category_id = :category_id')
+            ->setParameter('category_id', $category->getId());   
         }
 
         if(empty($name) === false){
-            $sql .= "WHERE a.name = '%:name%' ";
-            $parameter[] = ['name' => $name];
+            $queryBuilder = $queryBuilder->where('a.name LIKE :name')
+            ->setParameter('name', "%$name%");
         }
 
-        $sql .= "GROUP BY i.url";
-        $rsm = new ResultSetMapping();
-        $rsm->addEntityResult('App\Entity\Article', 'a');
-        //$rsm->addJoinedEntityResult('App\Entity\GlobalCategory', 'gc', 'a', 'global_category');
-        //$rsm->addJoinedEntityResult('App\Entity\Image', 'i', 'a', 'image');
-        $rsm->addFieldResult("a", "id", "id");
-        $rsm->addFieldResult("a", "name", "name");
-        $rsm->addFieldResult("a", "description", "description");
-        $rsm->addFieldResult("a", "uuid", "uuid");
-        //$rsm->addMetaResult("i", "url", "url");
-
-        /*$query = $entityManager->createNativeQuery($sql, $rsm);
-        foreach($parameter as $value){
-            //dd($value[0] . " " . $value[1]);
-            $query->setParameter($value[0], $value[1]);
-        }*/
-        $conn = $entityManager->getConnection();
-        $stmt = $conn->prepare($sql);
-        $stmt->execute($parameter);
-        //var_dump($stmt->fetchAll());
-        $articles = $query->getResult();
-        dd($articles);
-
+        $articles = $queryBuilder->getQuery()->getResult();
+        if(empty($articles)) {
+            return $this->json([
+                "success" => false,
+                "message" => "No Products founded!"
+            ], 500);
+        }
+        $items = [];
+        foreach($articles as $article){
+            $stock = ($article->getQuantity() > 0 && $article->getStock()) ? "stock" : "empty";
+            $image = $this->getDoctrine()->getRepository(Image::class)->findOneBy(["article_id" => $article->getId()]);
+            if(empty($image) === false){
+                $image = $image->getUrl();
+            }
+            $items[] = [
+                "id" => $article->getId(),
+                "name" => $article->getName(),
+                "price" => $article->getPrice(),
+                "description" =>  $article->getDescription(),
+                "stock" => $stock,
+                "uuid" => $article->getUuid(),
+                "image" => $image
+            ];
+        }
+        
+        return $this->json([
+            "success" => true,
+            "items" => $items
+        ], 200);
     }
 
     #[Route('/filter/name/{name}', name: 'filter_by_name', methods: ["GET", "HEAD"])]
@@ -65,7 +72,6 @@ class FilterController extends AbstractController
     {
         $name = trim($name);
         $entityManager = $this->getDoctrine()->getManager();
-        //$articles = $this->getDoctrine()->getRepository(Article::class)->findByName();
         $queryBuilder = $entityManager->createQueryBuilder();
 
         $queryBuilder->select('a')
