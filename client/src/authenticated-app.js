e7eb5f0205bec0924e6899b36a5d21dd3c4fade6
Mahamat Abakar <mahamat.abakar-hassan@epitech.eu>
2021-07-19 11:01:55 +0200

    appel de server sur le front

--- client/src/authenticated-app.js
+++ client/src/authenticated-app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Shop from './pages/Shop/Shop';
 
 
 const AuthenticatedApp = () => {
@@ -8,6 +9,7 @@ const AuthenticatedApp = () => {
         <Router>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/shop' component={Shop} />
             </Switch>
         </Router>
     );
--- client/src/pages/Shop/Shop.jsx
+++ client/src/pages/Shop/Shop.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 
 import Pagination from '../../components/Pagination/Pagination';
 import PreferenceAffichage from '../../components/Shop/PreferenceAffichage/PreferenceAffichage';
@@ -7,9 +7,36 @@ import Categories from '../../components/Shop/SlideBar/Categories/Catgefories';
 import Header from '../../components/CommonComponents/Header/Header';
 import Footer from '../../components/CommonComponents/Footer/index';
 import CartArticle from '../../components/CartArticle/CartArticle';
-
+import axios from "axios"
 
 const Shop = () => {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const user = localStorage.getItem("token");
+    const api = 'http://127.0.0.1:8000/article'; 
+    const token = user;
+    axios.get(api , { headers: {"Authorization" : `Bearer ${token}`} })
+    .then(res => {
+      setData(
+        <div className="users">
+          {res.data.items.map((user) => (
+             <CartArticle
+                image="assets/images/product/medium-size/1-7-270x300.jpg" //{data.image} pour l'instant on garde l'image par default
+                name={user.name}
+                prix={data.price}
+           />
+          ))}
+        </div>
+      );
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  },[]);
+
+
+
   return (
     <div className="main-wrapper">
       <Header/>
@@ -21,22 +48,15 @@ const Shop = () => {
                 <SearchBox />
                 <div className="widgets-area">
                   <Categories />
-
                 </div>
-
               </div>
             </div>
             <div className="col-xl-9 col-lg-8 order-1 order-lg-2">
               <PreferenceAffichage />
               <div className="tab-content">
-
                 <div className="tab-pane fade show active" id="grid-view" role="tabpanel" aria-labelledby="grid-view-tab">
                   <div className="product-grid-view row g-y-30">
-                    <CartArticle
-                      image="assets/images/product/medium-size/1-7-270x300.jpg"
-                      name="nom du produit"
-                      prix="30"
-                    />
+                {data}
                   </div>
                 </div>
                 {/*    <div className="tab-pane fade" id="list-view" role="tabpanel" aria-labelledby="list-view-tab">
@@ -46,7 +66,7 @@ const Shop = () => {
               </div> */}
               </div>
 
-              <Pagination />
+           
             </div>
           </div>
         </div>
