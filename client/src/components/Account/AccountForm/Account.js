ac777bea2e8f58ee05e15bdbdd1018373c25de73
Mahamat Abakar <mahamat.abakar-hassan@epitech.eu>
2021-07-20 01:25:35 +0200

    password component

--- client/src/components/Account/AccountForm/Account.js
+++ client/src/components/Account/AccountForm/Account.js
@@ -1,35 +1,100 @@
-import react from 'react'
+import React, { Component ,useState} from "react"
+import axios from "axios"
+
 
 const AccountForm = props =>{
+    const [newpassword, setNewPassword] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpassword, setConfirmPassword] = useState("");
+ 
+    const handleSubmit = async e => {
+          
+        e.preventDefault();
+
+        const users = {
+          password: password,
+          newpassword: newpassword
+        }
+
+        if(newpassword.length < 6){
+            alert(" Mot de passe trop court");
+        }
+        if(newpassword!=confirmpassword){
+            alert("Vos mots de passes sont pas identiques");
+        }
+      
+
+        const user = localStorage.getItem("token");
+        const api = 'http://206.81.25.252:8000/api/v1/user/edit/password'; 
+        const token = user;
+        var bodyFormData = new FormData();
+        bodyFormData.append('password', password);
+        bodyFormData.append('newpassword', newpassword);
+        axios({
+            method: "post",
+            url: api,
+            data: bodyFormData,
+            headers: { 
+               
+                "Authorization" : `Bearer ${token}`
+            },
+          })
+        .then(res => {
+        console.log(res);
+        })
+        .catch((e) => {
+        console.log(e.response)
+        });
+        console.log(token);
 
 
-    return(
-       
-          <form action="#" className="myaccount-form">
-            <div className="myaccount-form-inner">
-              <div className="single-input">
-                <label>Current Password(leave blank to leave
-                  unchanged)</label>
-                <input type="password" />
-              </div>
-              <div className="single-input">
-                <label>New Password (leave blank to leave
-                  unchanged)</label>
-                <input type="password" />
-              </div>
-              <div className="single-input">
-                <label>Confirm New Password</label>
-                <input type="password" />
-              </div>
-              <div className="single-input">
-                <button className="btn btn-custom-size lg-size btn-webshop-primary" type="submit">
-                  <span>SAVE CHANGES</span>
-                </button>
-              </div>
+    }
+
+     return (
+        
+        <div>
+            <br />
+            <div className="container">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="password"
+                            placeholder="password"
+                            onChange={event => setPassword(event.target.value)}
+                        />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="newpassword"
+                            placeholder="new password"
+                            onChange={event => setNewPassword(event.target.value)}
+                        />
+                    </div>
+                    <br />
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            name="confirmpassword"
+                            placeholder="confirm password"
+                            onChange={event => setConfirmPassword(event.target.value)}
+                        />
+                    </div>
+                    <br />
+                    <div>
+                        <button className="btn btn-login btn-outline-success" type="submit">
+                         SAVE 
+                        </button>
+                    </div>
+                </form>
             </div>
-          </form>
-         
-   
-    )
+            
+        </div>
+    );
 }
 export default AccountForm
