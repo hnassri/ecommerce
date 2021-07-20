36e90b8845cfd1941fdf258158c56a049873706c
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-07-20 21:20:16 +0200

    a

---
+++ client/src/context/panier.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import React from react ;
+
+
+
+const PanierProvider = ()=>{
+     
+const [Bag, setBag] = useState({})
+   
+   const addBag = ()=>{
+       
+   }
+}
+
+
