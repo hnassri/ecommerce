21189938d31ae694ea8367bc82da78a339a9f57a
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-07-26 09:46:53 +0200

    react_recode: show all articles in progress

--- front/src/UserPanel/components/Sidebar/index.js
+++ front/src/UserPanel/components/Sidebar/index.js
@@ -18,45 +18,39 @@ const SideBar = () => {
         });
     }, []);
     return (
-            <div className="shop-area section-space-y-axis-100">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xl-3 col-lg-4 order-2 order-lg-1 pt-5 pt-lg-0">
-                            <div className="sidebar-area">
-                                <div className="widgets-searchbox">
-                                    <form id="widgets-searchbox">
-                                        <input className="input-field" type="text" placeholder="Search"/>
-                                        <button className="widgets-searchbox-btn" type="submit">
-                                            <i className="fa fa-search"></i>
-                                        </button>
-                                    </form>
-                                </div>
-                                <div className="widgets-area">
-                                    <div className="widgets-item pt-0">
-                                        <h2 className="widgets-title mb-4">Catégorie</h2>
-                                        <ul className="widgets-category">
-                                            <li>
-                                                <Link>
-                                                    <i className="fa fa-chevron-right"></i>&nbsp;Toute Catégorie
-                                                </Link>
-                                            </li>
-                                            {categories.map((value,index) => {
-                                                return(
-                                                    <li key={index}>
-                                                        <Link>
-                                                            <i className="fa fa-chevron-right"></i>&nbsp;{value.name}    
-                                                        </Link>
-                                                    </li>
-                                                )
-                                            })}
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div className="col-xl-3 col-lg-4 order-2 order-lg-1 pt-5 pt-lg-0">
+            <div className="sidebar-area">
+                <div className="widgets-searchbox">
+                    <form id="widgets-searchbox">
+                        <input className="input-field" type="text" placeholder="Search"/>
+                        <button className="widgets-searchbox-btn" type="submit">
+                            <i className="fa fa-search"></i>
+                        </button>
+                    </form>
+                </div>
+                <div className="widgets-area">
+                    <div className="widgets-item pt-0">
+                        <h2 className="widgets-title mb-4">Catégorie</h2>
+                        <ul className="widgets-category">
+                            <li>
+                                <Link>
+                                    <i className="fa fa-chevron-right"></i>&nbsp;Toute Catégorie
+                                </Link>
+                            </li>
+                            {categories.map((value,index) => {
+                                return(
+                                    <li key={index}>
+                                        <Link>
+                                            <i className="fa fa-chevron-right"></i>&nbsp;{value.name}    
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
 
--- front/src/UserPanel/pages/Shop/index.js
+++ front/src/UserPanel/pages/Shop/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import BreadCrumb from "../../components/BreadCrumb";
+import ProductCart from "../../components/ProductCart";
 import SideBar from "../../components/Sidebar";
 
 const Shop = () => {
     return (
         <div className="main-content">
             <BreadCrumb name="Magasin"/>
-            <SideBar/>
+            <div class="shop-area section-space-y-axis-100">
+                <div class="container">
+                    <div class="row">
+                        <SideBar/>
+                        <ProductCart />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
