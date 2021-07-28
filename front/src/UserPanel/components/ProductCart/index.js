7359bb388210366afc57a76e6b5d10248335a029
NASSRI Hamdi <nassrihamdi@gmail.com>
2021-07-28 11:10:29 +0200

    react_recode: get all articles done

--- front/src/UserPanel/components/ProductCart/index.js
+++ front/src/UserPanel/components/ProductCart/index.js
@@ -1,46 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ProductCart = () => {
+const ProductCart = ({article}) => {
     return (
-        <div class="col-xl-9 col-lg-8 order-1 order-lg-2">
-            <div class="tab-content">
-                <div class="tab-pane fade show active" id="grid-view" role="tabpanel" aria-labelledby="grid-view-tab">
-                    <div class="product-grid-view row g-y-20">
-                        <div class="col-md-4 col-sm-6">
-                            <div class="product-item">
-                                <div class="product-img">
-                                    <a href="single-product-variable.html">
-                                        <img class="primary-img" src="assets/images/product/medium-size/1-1-270x300.jpg" alt="Product Images"/>
-                                        <img class="secondary-img" src="assets/images/product/medium-size/1-2-270x300.jpg" alt="Product Images"/>
-                                    </a>
-                                    <div class="product-add-action">
-                                        <ul>
-                                            <li>
-                                                <a href="wishlist.html" data-tippy="Add to wishlist" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">
-                                                    <i class="pe-7s-like"></i>
-                                                </a>
-                                            </li>
-                                            <li class="quuickview-btn" data-bs-toggle="modal" data-bs-target="#quickModal">
-                                                <a href="#" data-tippy="Quickview" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">
-                                                    <i class="pe-7s-look"></i>
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a href="cart.html" data-tippy="Add to cart" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder">
-                                                    <i class="pe-7s-cart"></i>
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div class="product-content">
-                                    <a class="product-name" href="single-product-variable.html">Nom du produit</a>
-                                    <div class="price-box pb-1">
-                                        <span class="new-price">(Prix)</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div className="col-md-4 col-sm-6">
+            <div className="product-item">
+                <div className="product-img">
+                    <Link to={"/article/" + article.id}>
+                        <img className="primary-img" src={"http://206.81.25.252:8000/" + article.image} alt="Product Images"/>
+                    </Link>
+                </div>
+                <div className="product-content">
+                    <Link to={"/article/" + article.id} className="product-name" >{article.name}</Link>
+                    <div className="price-box pb-1">
+                        <span className="new-price">{article.price}</span>
                     </div>
                 </div>
             </div>
--- front/src/UserPanel/pages/Shop/index.js
+++ front/src/UserPanel/pages/Shop/index.js
@@ -1,17 +1,47 @@
-import React from "react";
+import axios from "../../../axios/axios";
+import React, { useEffect, useState } from "react";
 import BreadCrumb from "../../components/BreadCrumb";
 import ProductCart from "../../components/ProductCart";
 import SideBar from "../../components/Sidebar";
 
 const Shop = () => {
+    const [articles, setArticles] = useState([]);
+    const getArticles = () => {
+        axios.get("/article")
+        .then(res => {
+            if(res.data.success === true){
+                setArticles(res.data.items);
+            }
+        })
+        .catch(e => {
+            if(e.response.data.success === false){
+                alert(e.response.data.message);
+            }else{
+                alert("An error occured");
+            }
+        })
+    }
+    useEffect(() => {
+        getArticles();
+    }, []);
     return (
         <div className="main-content">
             <BreadCrumb name="Magasin"/>
-            <div class="shop-area section-space-y-axis-100">
-                <div class="container">
-                    <div class="row">
+            <div className="shop-area section-space-y-axis-100">
+                <div className="container">
+                    <div className="row">
                         <SideBar/>
-                        <ProductCart />
+                        <div className="col-xl-9 col-lg-8 order-1 order-lg-2">
+                            <div className="tab-content">
+                                <div className="tab-pane fade show active" id="grid-view" role="tabpanel" aria-labelledby="grid-view-tab">
+                                    <div className="product-grid-view row g-y-20">
+                                        {articles.map((value, index) => {
+                                            return <ProductCart article={value} key={index}/>;
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
